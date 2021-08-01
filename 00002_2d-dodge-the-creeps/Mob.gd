8147bb283295f67f2207eb6b12e3f1634d297298
MRW Neundorf <ninevillage@gmail.com>
2021-08-01 20:55:00 +0200

    feat(00002): added mob scene

---
+++ 00002_2d-dodge-the-creeps/Mob.gd
@@ -0,0 +1,10 @@
+extends RigidBody2D
+
+export var min_speed = 150.0
+export var max_speed = 250.0
+
+
+func _ready():
+	$AnimatedSprite.playing = true
+	var mob_types = $AnimatedSprite.frames.get_animation_names()
+	$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
